85e0e94908949ece8e204e879c86b0098e17fb28
jkaylight <jkaylight@gmail.com>
2024-04-28 20:01:47 +0100

    Emit event for front-end

--- Escrow.sol
+++ Escrow.sol
@@ -5,20 +5,7 @@ contract Escrow {
     address public depositor;
     address public beneficiary;
     address public arbiter;
-
-    constructor(address _arbiter, address _beneficiary) payable {
-        arbiter = _arbiter;
-        beneficiary = _beneficiary;
-        depositor = msg.sender;
-    }
-
-    // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
-
-contract Escrow {
-    address public depositor;
-    address public beneficiary;
-    address public arbiter;
+    event Approved(uint);
     
     constructor(address _arbiter, address _beneficiary) payable {
         arbiter = _arbiter;
@@ -31,10 +18,11 @@ contract Escrow {
     function approve() external {
         if(msg.sender != arbiter) revert NotAuthorized();
         
-        (bool success, ) = beneficiary.call{ value: address(this).balance }("");
+        uint balance = address(this).balance;
+        (bool success, ) = beneficiary.call{ value: balance }("");
         require(success);
+        emit Approved(balance);
     }
 
 
-}
 }
