92efc6db0250ba026e44e316091775a92a15ec17
jkaylight <jkaylight@gmail.com>
2024-04-28 19:58:02 +0100

    Only arbiter can call approve()

--- Escrow.sol
+++ Escrow.sol
@@ -12,8 +12,29 @@ contract Escrow {
         depositor = msg.sender;
     }
 
-    function approve() external payable {
-        (bool success, ) = beneficiary.call{value: address(this).balance}("");
+    // SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Escrow {
+    address public depositor;
+    address public beneficiary;
+    address public arbiter;
+    
+    constructor(address _arbiter, address _beneficiary) payable {
+        arbiter = _arbiter;
+        beneficiary = _beneficiary;
+        depositor = msg.sender;
+    }
+    
+    error NotAuthorized();
+
+    function approve() external {
+        if(msg.sender != arbiter) revert NotAuthorized();
+        
+        (bool success, ) = beneficiary.call{ value: address(this).balance }("");
         require(success);
     }
+
+
+}
 }
